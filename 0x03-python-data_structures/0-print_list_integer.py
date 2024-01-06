4c73bd256e27f122f9b611a639fd3094cd1f8ef1
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-06 18:31:42 +0100

    commits

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
 
-# Test the function
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+def print_list_integer(my_list=[]):
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
