7f17ed4255c6bb7c65a60adb162aee2261457540
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:11:20 +0100

    TAsk 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     elements_printed = 0
+
     try:
         for i in range(x):
             print(my_list[i], end="")
             elements_printed += 1
-        print()
 
     except IndexError:
+        # Handle the case where the index is out of range
+        pass
+
+    finally:
+        # Print a new line after all elements are printed or in case of an error
         print()
-    return (elements_printed)
+
+    return elements_printed
