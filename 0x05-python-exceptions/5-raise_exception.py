69252db0862e399a61e024ea2d1546288a66ae28
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:56:45 +0100

    5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise TypeError("This is a type exception")
-    except TypeError as e:
-        print(f"Caught a type exception: {e}")
+    raise TypeError("This is a type exception")
