377d3e8a5cf530317fad0a425ec6ea2b5a56fc9c
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:39:05 +0100

    T 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     integers_printed = 0
-    
+
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -10,5 +10,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
     print()
-
     return integers_printed
