531c015ef368a96e9df1217ace7cd9b04cdcb65b
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 16:45:02 +0100

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    elements_printed = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            elements_printed += 1
+        print()
+
+    except IndexError:
+        print()
+
+        return elements_printed
