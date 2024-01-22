a186df860a3019c14911ba9b4c9395026b669e91
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 18:07:21 +0100

    Task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        print()
+        return True
+    except Exception as e:
+        import sys
+        print("Exception:", e, file=sys.stderr)
+        return False
