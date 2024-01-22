af24c405bd4e138515df6e3255fcf955ab258f0e
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:26:15 +0100

    task 2 error fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 
-
 def safe_print_list_integers(my_list=[], x=0):
     integers_printed = 0
 
     try:
         for i in range(x):
-            # Check if the current element is an integer
             if type(my_list[i]) is int:
-                # Use "{:d}".format() to print the integer value
                 print("{:d}".format(my_list[i]), end=" ")
                 integers_printed += 1
 
-        # Print a new line after all integers are printed
         print()
 
     except (ValueError, TypeError, IndexError):
-        # Handle the cases where there is a non-integer value or index is out of range
         pass
 
     return (integers_printed)
