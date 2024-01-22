e49da01b335a1e6b424f59041831d42af0fa0e4b
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:33:10 +0100

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
             integers_printed += 1
         except (ValueError, TypeError, IndexError):
             pass
-        print()
+    print()
 
     return integers_printed
