1081fcb5ca2268bc5537918276092ab68c95cd11
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-22 17:12:54 +0100

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     elements_printed = 0
 
@@ -10,11 +9,9 @@ def safe_print_list(my_list=[], x=0):
             elements_printed += 1
 
     except IndexError:
-        # Handle the case where the index is out of range
         pass
 
     finally:
-        # Print a new line after all elements are printed or in case of an error
         print()
 
-    return elements_printed
+    return (elements_printed)
