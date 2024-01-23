4deb481aefbac4a02b626d60c33877a771bbad12
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-23 17:14:24 +0100

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,6 @@ class Square:
     This is the Square class.
     It represents a square and can be used as a template
     for creating square objects.
-    Attributes:
-        size (int): Private instance attribute representing the size of the square.
     """
 
     def __init__(self, size=0):
@@ -20,7 +18,7 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
