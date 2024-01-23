e60bef8d9969ef412fbc3263834e3a1ff11c02fb
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-23 16:54:44 +0100

    TAsk 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+
+    It represents a square and can be used as a template
+    for creating square objects.
+
+    Attributes:
+        No attributes for an empty class.
+
+    Methods:
+        No methods for an empty class.
+    """
+    pass
