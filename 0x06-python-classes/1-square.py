bb1e99844fd94a3078a39c4c4da8bdb43bc2d497
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-23 17:05:59 +0100

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,13 +2,14 @@
 
 """Defiining a class Square"""
 
+
+
 class Square:
     """
     This is the Square class.
     It represents a square and can be used as a template
     for creating square objects.
     """
-
     def __init__(self, size):
         """
         Initializes a new instance of the Square class.
