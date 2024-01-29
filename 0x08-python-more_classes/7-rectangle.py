2803e2a1f36d2c7a2572a7ba5360ca843f2cb0b9
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-29 13:56:33 +0100

    fix 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,37 +1,57 @@
 #!/usr/bin/python3
-"""Rectangle module"""
+"""Module 7-rectangle
+"""
 
 
 class Rectangle:
-    """Rectangle class with private width and height attributes"""
+    """Rectangle class defination."""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialization method with optional width and height"""
+        """Initializes a Rectangle instance in a contructor."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
+    def __str__(self):
+        """Returns an informal string representation"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        rectangle_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle_str += str(self.print_symbol)
+            rectangle_str += "\n"
+        return rectangle_str[:-1]
+
+    def __repr__(self):
+        """Return internal string representation"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Fire when a rectangle instance is destroyed."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
     @property
     def width(self):
-        """Gets method for width"""
+        """Retrieves the width of a Rectangle instance."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets method for width with validation"""
+        """Sets the width of a Rectangle instance"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Gets method for height"""
+        """Retrieves the height of a Rectangle instance."""
         return self.__height
 
     @height.setter
@@ -44,7 +64,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Calculate and return the rectangle area"""
+        """Calculates the area of a Rectangle instance"""
         return self.__width * self.__height
 
     def perimeter(self):
@@ -52,19 +72,3 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """Return a string representation of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        else:
-            return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
-
-    def __repr__(self):
-        """Return a string representation of the rectangle for recreation"""
-        return f"Rectangle({self.__width}, {self.__height})"
-
-    def __del__(self):
-        """Print a message when an instance is deleted and decrement instances"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
